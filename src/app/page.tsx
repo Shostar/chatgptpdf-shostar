3a2836c30e27701c42debb762d80782d3cec05a1
shostart <horacioshougocelisyoshikawa000@gmail.com>
2024-03-14 03:26:06 -0600

    falta stripe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { UserButton,auth } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { LogIn } from 'lucide-react'
-import FileUpload from '@/components/ui/FileUpload'
+import FileUpload from '@/components/FileUpload'
 
 export default async function Home() {
   const {userId} = await auth()
